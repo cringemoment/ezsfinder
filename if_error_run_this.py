2bc7ad504a9dea55e61e3fb8fd88172ba0e61770
cringemoment <69696899+cringemoment@users.noreply.github.com>
2023-03-31 23:34:34 -0400

    you got an error? run this

---
+++ if_error_run_this.py
@@ -0,0 +1,5 @@
+from os import system
+system("pip install beautifulsoup4 > ezsfinder.txt")
+system("npm install -g sfinder-strict-minimal > ezsfinder.txt")
+system("npm install tetris-fumen > ezsfinder.txt")
+system("npm install yargs")
