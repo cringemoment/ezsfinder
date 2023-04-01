a80308e76f64916bb54a53ba3fe9c125ecd9bc6d
cringemoment <69696899+cringemoment@users.noreply.github.com>
2023-03-31 23:36:50 -0400

    added some reassurance

--- if_error_run_this.py
+++ if_error_run_this.py
@@ -1,5 +1,6 @@
 from os import system
-system("pip install beautifulsoup4 > ezsfinder.txt")
-system("npm install -g sfinder-strict-minimal > ezsfinder.txt")
-system("npm install tetris-fumen > ezsfinder.txt")
+print("hey, dont worry. this is just going to install all the required modules not built in for you. nothing to worry about.")
+system("pip install beautifulsoup4")
+system("npm install -g sfinder-strict-minimal")
+system("npm install tetris-fumen")
 system("npm install yargs")
