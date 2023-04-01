29a92291c79544818a686b18a6eac517ddfb70d2
cringemoment <69696899+cringemoment@users.noreply.github.com>
2023-03-31 20:02:04 -0400

    added special_minimals function

--- ezsfinder.py
+++ ezsfinder.py
@@ -1,11 +1,13 @@
 from sys import argv
 from os import system
+from bs4 import BeautifulSoup
 
 help = """The options are:
 ezsfinder.py chance <fumen> <queue> <clear=4>; This spits out the chance to solve this.
 ezsfinder.py fail_queues <fumen> <queue> <clear=4>; This spits out all the fail queues.
 ezsfinder.py minimals <fumen> <queue> <clear=4> <saves=all of them>; This spits out a tinyurl for the minimals. Saves is by default not considered.
 ezsfinder.py score <fumen> <queue> <clear=4> <initial_b2b=false> <initial_combo=0> <b2b_bonus=0>; This gives you information and the average score for the setup.
+ezsfinder.py special_minimals <fumen> <queue> <clear=4> <saves=all of them>; This gets you minimal sets for t-spin and tetris solves.
 ezsfinder.py help <command>; This gives you more specific help for specific commands.
 ezsfinder.py help queue; This gives you more information on how the queue notation works."""
 
@@ -15,8 +17,8 @@ if(len(argv) < 3):
     raise SystemExit(0)
 
 if(not pargv[1] == "help"):
-    argv = [pargv[0], pargv[1], pargv[2], pargv[3], 4, "I||O||J||L||T||S||Z", "false", 0, 0]
-    commands = [0, 0, 0, 0, "clear", "saves", "initial_b2b", "initial_combo", "b2b_bonus"]
+    argv = [pargv[0], pargv[1], pargv[2], pargv[3], 4, "I||O||J||L||T||S||Z", "false", 0, 0, "tss"]
+    commands = [0, 0, 0, 0, "clear", "saves", "initial_b2b", "initial_combo", "b2b_bonus", "minimal_type"]
     for i in pargv:
         if("=" in i):
             arg = i.split("=")
@@ -43,7 +45,7 @@ elif(argv[1].lower() == "fail_queues"):
 
 elif(argv[1].lower() == "minimals"):
     system("java -jar sfinder.jar path -f csv -k pattern --tetfu %s --patterns %s --clear %s > ezsfinder.txt" % (argv[2], argv[3], argv[4]))
-    system('py sfinder-saves.py filter -w "%s" -p "%s" > ezsfinder.txt' % (argv[5], argv[3]))
+    system('py sfinder-saves.py filter -w "%s" -p "%s" > ezsfinder.txt ' % (argv[5], argv[3]))
 
 elif(argv[1].lower() == "score"):
     system("java -jar sfinder.jar path -t %s -p %s --clear %s --hold avoid -split yes -f csv -k pattern -o output/path.csv > ezsfinder.txt" % (argv[2], argv[3], argv[4]))
@@ -59,6 +61,18 @@ elif(argv[1].lower() == "score"):
             print("On average, when the setup has a perfect clear, you would score %s points."% round(float(i.split(": ")[1][:-1]), 2))
             print("Factoring in pc chance, the average score is %s" % round(float(i.split(": ")[1][:-1]) / int(score[-1]) * int(score[v + 1].split(": ")[1][:-1]), 2))
 
+elif(argv[1].lower() == "special_minimals" or argv[1].lower() == "special-minimals"):
+    system("java -jar sfinder.jar path -t %s -p %s --clear %s > ezsfinder.txt" % (argv[2], argv[3], argv[4]))
+    with open('output/path_unique.html', encoding = "utf-8") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    fumen = soup.find('a')['href']
+    system("node glueFumens.js %s > input/field.txt" % fumen)
+    system("java -jar sfinder.jar cover -p %s -M %s > ezsfinder.txt" % (argv[3], argv[9]))
+    system("cover-to-path.py > ezsfinder.txt")
+    system("sfinder-minimal output/cover_to_path.csv> ezsfinder.txt")
+    system("true_minimal.py")
+
 elif(argv[1].lower() == "help"):
     if(argv[2].lower() == "chance"):
         print("""This uses sfinder's percent command to calculate the score.The command structure is sfinder chance <fumen> <queue>.
@@ -69,6 +83,15 @@ ezsfinder chance v115@9gA8IeB8CeA8DeF8DeF8NeAgH *p7. (Outputs 99.76%)""")
 When you see the square brackets, it indicates a bag. [SZ] stands for a bag, composing of an S and a Z piece. * Stands in for a full bag of all the pieces, or [IOSZJLT].
 When you see a p and then a number next to it, it indicates how many bags to draw out of that queue. [JLT]p2 indicates to draw 2 pieces out of the bag. This is fed into the program as all ways to draw 2 pieces out of the bag [JLT], so the program would get:
 JL,JT,LT.""")
+    if(argv[2].lower() == "special-minimals"):
+        print("""This hooks together like 5 different programs to get the minimals. Your options for minimal_type are the same as sfinder's cover --mode parameters, which are:
+tsm: This includes all tspins
+tss: This includes tss and up
+tsd: This includes all tsd and up
+tst: This includes only tst
+tetris: Looks for tetris anywhere in the solve
+tetris-end: Looks for the last 4 lines cleared being a tetris
+""")
 
 else:
     print(help)
