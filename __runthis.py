7247e76de252312698b135d19a199fb5f5723625
cringemoment <69696899+cringemoment@users.noreply.github.com>
2023-04-07 10:33:45 -0400

    Update __runthis.py

--- __runthis.py
+++ __runthis.py
@@ -1,6 +1,7 @@
 from os import system
 print("hey, dont worry. this is just going to install all the required modules not built in for you. nothing to worry about.")
 system("pip install beautifulsoup4")
+system("pip install argparse")
 system("npm install -g sfinder-strict-minimal")
 system("npm install tetris-fumen")
 system("npm install yargs")
